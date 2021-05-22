645a534bfd1548e9af05a068a1c16afd92bdeb75
minh-an-luu <0229lumi@jonkoping.se>
2021-05-22 22:18:48 +0200

    chart js

--- script.js
+++ script.js
@@ -1,4 +1,64 @@
-$(document).ready(function () {
+let myChart = document.getElementById('myChart').getContext('2d');
 
- 
+// Global Options
+Chart.defaults.global.defaultFontFamily = 'Mukta';
+Chart.defaults.global.defaultFontSize = 18;
+Chart.defaults.global.defaultFontColor = '#777';
+
+let massPopChart = new Chart(myChart, {
+  type:'horizontalBar', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+  data:{
+    labels:['Bilar Kör', 'Atteviks', 'Hermans bilar', 'Haralds cykel', 'Åverts familje bil', 'Alfons husbil'],
+    datasets:[{
+      label:'Sannolikheten för samlag',
+      data:[
+        100,
+        90,
+        80,
+        50,
+        62,
+        3
+      ],
+     
+      //backgroundColor:'green',
+      backgroundColor:[
+        'rgba(255, 99, 132, 0.5)',
+        'rgba(54, 162, 235, 0.5)',
+        'rgba(255, 206, 86, 0.5)',
+        'rgba(75, 192, 192, 0.5)',
+        'rgba(153, 102, 255, 0.5)',
+        'rgba(255, 159, 64, 0.5)',
+        'rgba(255, 99, 132, 0.5)'
+      ],
+      borderWidth:1,
+      borderColor:'#777',
+      hoverBorderWidth:2,
+      hoverBorderColor:'#000'
+    }]
+  },
+  options:{
+    title:{
+      display:true,
+      text:'Hur ofta du kommer att ha samlag om du köper från Bilar Kör',
+      fontSize:25
+    },
+    legend:{
+      display:true,
+      position:'right',
+      labels:{
+        fontColor:'#000'
+      }
+    },
+    layout:{
+      padding:{
+        left:50,
+        right:0,
+        bottom:0,
+        top:0
+      }
+    },
+    tooltips:{
+      enabled:true
+    }
+  }
 });
