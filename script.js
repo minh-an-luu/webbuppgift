a2426efeae926d3039cc483e1df3c2c2d1af2fb9
Minh An Luu <0229lumi@jonkoping.se>
2021-05-23 23:19:59 +0200

    Köp meny

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let myChart = document.getElementById('myChart').getContext('2d');
 
-// Global Options
+
 Chart.defaults.global.defaultFontFamily = 'Mukta';
-Chart.defaults.global.defaultFontSize = 18;
+Chart.defaults.global.defaultFontSize = 15;
 Chart.defaults.global.defaultFontColor = '#777';
 
 let massPopChart = new Chart(myChart, {
-  type:'horizontalBar', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+  type:'bar', 
   data:{
     labels:['Bilar Kör', 'Atteviks', 'Hermans bilar', 'Haralds cykel', 'Åverts familje bil', 'Alfons husbil'],
     datasets:[{
@@ -20,7 +20,33 @@ let massPopChart = new Chart(myChart, {
         3
       ],
      
-      //backgroundColor:'green',
+      
+      backgroundColor:[
+        'rgba(15, 9, 12, 0.5)',
+        'rgba(2, 12, 135, 0.5)',
+        'rgba(55, 06, 86, 0.5)',
+        'rgba(5, 92, 12, 0.5)',
+        'rgba(253, 02, 255, 0.5)',
+        'rgba(205, 59, 64, 0.5)',
+        'rgba(155, 9, 132, 0.5)'
+      ],
+      borderWidth:1,
+      borderColor:'#777',
+      hoverBorderWidth:2,
+      hoverBorderColor:'#000'
+    }
+    ,{
+      label:'Coolhets faktor',
+      data:[
+        100,
+        10,
+        60,
+        10,
+        6,
+        80
+      ],
+     
+      
       backgroundColor:[
         'rgba(255, 99, 132, 0.5)',
         'rgba(54, 162, 235, 0.5)',
@@ -39,7 +65,7 @@ let massPopChart = new Chart(myChart, {
   options:{
     title:{
       display:true,
-      text:'Hur ofta du kommer att ha samlag om du köper från Bilar Kör',
+      text:'Märkvärdig statestik',
       fontSize:25
     },
     legend:{
@@ -61,4 +87,40 @@ let massPopChart = new Chart(myChart, {
       enabled:true
     }
   }
+}); //Chart js kod 
+
+//Kod för köp menu
+
+$('.minus-btn').on('click', function(e) {
+  e.preventDefault();
+  var $this = $(this);
+  var $input = $this.closest('div').find('input');
+  var value = parseInt($input.val());
+
+  if (value > 1) {
+    value = value - 1;
+  } else {
+    value = 0;
+  }
+
+  $input.val(value);
+
 });
+
+$('.plus-btn').on('click', function(e) {
+  e.preventDefault();
+  var $this = $(this);
+  var $input = $this.closest('div').find('input');
+  var value = parseInt($input.val());
+
+  if (value < 100) {
+    value = value + 1;
+  } else {
+    value =100;
+  }
+
+  $input.val(value);
+});
+
+
+
